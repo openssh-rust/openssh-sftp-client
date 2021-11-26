d34f0bf5b9951299a8e8865cf2146fcf419cb619
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-26 23:56:08 +1100

    Impl `Client::new`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,3 +1,5 @@
+use tokio_pipe::{PipeRead, PipeWrite};
+
 mod read_end;
 mod threadsafe_waker;
 mod write_end;
@@ -6,4 +8,12 @@ mod write_end;
 pub struct Client {
     write_end: write_end::WriteEnd,
     read_end: read_end::ReadEnd,
+}
+impl Client {
+    fn new(reader: PipeRead, writer: PipeWrite) -> Self {
+        Self {
+            read_end: read_end::ReadEnd::new(reader),
+            write_end: write_end::WriteEnd::new(writer),
+        }
+    }
 }
