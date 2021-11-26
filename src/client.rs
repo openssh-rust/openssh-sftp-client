fb20522721ed984411b32735c9158a673f522a32
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-26 22:51:33 +1100

    Impl client::WriteEnd
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/client.rs
@@ -0,0 +1,63 @@
+use core::future::Future;
+use core::pin::Pin;
+use core::task::{Context, Poll};
+
+use std::io;
+
+use parking_lot::RwLock;
+
+use tokio::io::AsyncWriteExt;
+use tokio_pipe::{AtomicWriteBuffer, PipeRead, PipeWrite};
+
+#[derive(Debug)]
+struct WriteEnd(RwLock<PipeWrite>);
+
+impl WriteEnd {
+    fn new(writer: PipeWrite) -> Self {
+        Self(RwLock::new(writer))
+    }
+
+    async fn write_atomic(&self, buf: AtomicWriteBuffer<'_>, len: usize) -> io::Result<()> {
+        struct AtomicWriteFuture<'pipe, 'a>(&'pipe PipeWrite, Option<AtomicWriteBuffer<'a>>);
+
+        impl<'pipe, 'a> Future for AtomicWriteFuture<'pipe, 'a> {
+            type Output = io::Result<usize>;
+
+            fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+                Pin::new(self.0).poll_write_atomic(cx, self.1.take().unwrap())
+            }
+        }
+
+        let bytes = AtomicWriteFuture(&*self.0.read(), Some(buf)).await?;
+        if bytes != len {
+            Err(io::Error::new(
+                io::ErrorKind::Other,
+                "tokio_pipe::PipeWrite::poll_write_atomic isn't atomic",
+            ))
+        } else {
+            Ok(())
+        }
+    }
+
+    async fn write_locked(&self, buf: &[u8]) -> io::Result<()> {
+        self.0.write().write_all(buf).await
+    }
+
+    async fn write(&self, buf: &[u8]) -> io::Result<()> {
+        match AtomicWriteBuffer::new(buf) {
+            Some(atomic_buf) => self.write_atomic(atomic_buf, buf.len()).await,
+            None => self.write_locked(buf).await,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ReadEnd {
+    reader: PipeRead,
+}
+
+#[derive(Debug)]
+pub struct Client {
+    write_end: WriteEnd,
+    read_end: ReadEnd,
+}
