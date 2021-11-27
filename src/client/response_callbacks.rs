99ced1c0311b6775656b2de539cafc4fdf97face
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-27 15:35:16 +1100

    Fix ResponseCallbacks: Use RwLock to guard `Arena`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/client/response_callbacks.rs
+++ src/client/response_callbacks.rs
@@ -1,8 +1,9 @@
 use super::{ResponseCallback, ThreadSafeWaker};
 
+use parking_lot::RwLock;
 use thunderdome::Arena;
 
 #[derive(Debug, Default)]
-pub(crate) struct ResponseCallbacks(Arena<(ThreadSafeWaker, ResponseCallback)>);
+pub(crate) struct ResponseCallbacks(RwLock<Arena<(ThreadSafeWaker, ResponseCallback)>>);
 
 impl ResponseCallbacks {}
