4de46ebea5304fabb2c6d776cad9af4c4249ca3b
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-27 20:16:49 +1100

    Add prototype `enum Request`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 mod client;
 mod constants;
 mod error;
+mod request;
+
+use request::Request;
 
 pub use client::Client;
 pub use error::Error;
---
+++ src/request.rs
@@ -0,0 +1,2 @@
+#[derive(Debug)]
+pub(crate) enum Request {}
