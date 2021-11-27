84cf510a7d6146770bbb76eed9162f29c84b417f
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-27 12:33:08 +1100

    Refactor client: Move `response_callbacks` and `request_id` into `Client`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,23 +1,73 @@
+use core::sync::atomic::{AtomicU32, Ordering};
+use std::io;
+
+use dashmap::DashMap;
+
 use tokio_pipe::{PipeRead, PipeWrite};
 
 mod read_end;
 mod threadsafe_waker;
 mod write_end;
 
+use read_end::ReadEnd;
+use threadsafe_waker::ThreadSafeWaker;
+use write_end::WriteEnd;
+
+/// Prototype
+#[derive(Debug)]
+struct ResponseCallback {}
+
+impl ResponseCallback {
+    /// reader is used to read additional variable length data, especially
+    /// one that can be very long (response body of read request).
+    ///
+    /// Return true if the callback is already called and should be removed.
+    async fn call(&mut self, response: u8, reader: CountedReader<'_>) -> io::Result<bool> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+struct CountedReader<'a>(&'a PipeRead, usize);
+impl CountedReader<'_> {
+    fn get_bytes_left(&self) -> usize {
+        self.1
+    }
+
+    /// Read at most get_bytes_left()
+    fn read(&mut self, len: usize) -> io::Result<usize> {
+        todo!()
+    }
+}
+impl Drop for CountedReader<'_> {
+    fn drop(&mut self) {
+        // consume all bytes left readable
+    }
+}
+
 #[derive(Debug)]
 pub struct Client {
-    write_end: write_end::WriteEnd,
-    read_end: read_end::ReadEnd,
+    write_end: WriteEnd,
+    read_end: ReadEnd,
+
+    response_callbacks: DashMap<u32, (ThreadSafeWaker, ResponseCallback)>,
+    request_id: AtomicU32,
 }
 impl Client {
     async fn negotiate(reader: &mut PipeRead, writer: &mut PipeWrite) {}
+
+    fn get_request_id(&self) -> u32 {
+        self.request_id.fetch_add(1, Ordering::Relaxed)
+    }
 
     pub async fn connect(mut reader: PipeRead, mut writer: PipeWrite) -> Self {
         Self::negotiate(&mut reader, &mut writer).await;
 
         Self {
-            read_end: read_end::ReadEnd::new(reader),
-            write_end: write_end::WriteEnd::new(writer),
+            read_end: ReadEnd::new(reader),
+            write_end: WriteEnd::new(writer),
+            response_callbacks: DashMap::new(),
+            request_id: AtomicU32::new(0),
         }
     }
 }
--- src/client/read_end.rs
+++ src/client/read_end.rs
@@ -1,66 +1,18 @@
-use super::threadsafe_waker::ThreadSafeWaker;
-
-use core::sync::atomic::{AtomicU32, Ordering};
-
-use std::io;
-
 use serde::Deserialize;
 use ssh_format::from_bytes;
 
-use dashmap::DashMap;
-
 use tokio_pipe::PipeRead;
-
-/// Prototype
-#[derive(Debug)]
-struct ResponseCallback {}
-
-impl ResponseCallback {
-    /// reader is used to read additional variable length data, especially
-    /// one that can be very long (response body of read request).
-    ///
-    /// Return true if the callback is already called and should be removed.
-    async fn call(&mut self, response: u8, reader: CountedReader<'_>) -> io::Result<bool> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
-struct CountedReader<'a>(&'a PipeRead, usize);
-impl CountedReader<'_> {
-    fn get_bytes_left(&self) -> usize {
-        self.1
-    }
-
-    /// Read at most get_bytes_left()
-    fn read(&mut self, len: usize) -> io::Result<usize> {
-        todo!()
-    }
-}
-impl Drop for CountedReader<'_> {
-    fn drop(&mut self) {
-        // consume all bytes left readable
-    }
-}
 
 #[derive(Debug)]
 pub(crate) struct ReadEnd {
     reader: PipeRead,
     buffer: Vec<u8>,
-    response_callbacks: DashMap<u32, (ThreadSafeWaker, ResponseCallback)>,
-    request_id: AtomicU32,
 }
 impl ReadEnd {
     pub(crate) fn new(reader: PipeRead) -> Self {
         Self {
             reader,
             buffer: Vec::new(),
-            response_callbacks: DashMap::new(),
-            request_id: AtomicU32::new(0),
         }
-    }
-
-    fn get_request_id(&self) -> u32 {
-        self.request_id.fetch_add(1, Ordering::Relaxed)
     }
 }
