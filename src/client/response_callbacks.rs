9f17ce690b15a13023b91290a19be0d3ca21b82a
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-27 15:45:45 +1100

    Impl `ResponseCallbacks::{insert, do_callback, remove}`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/client/response_callbacks.rs
+++ src/client/response_callbacks.rs
@@ -1,9 +1,49 @@
-use super::{ResponseCallback, ThreadSafeWaker};
+use super::{CountedReader, ResponseCallback, ThreadSafeWaker};
+
+use std::io;
+use std::sync::Arc;
 
 use parking_lot::RwLock;
 use thunderdome::Arena;
 
-#[derive(Debug, Default)]
-pub(crate) struct ResponseCallbacks(RwLock<Arena<(ThreadSafeWaker, ResponseCallback)>>);
+pub(crate) type Value = (ThreadSafeWaker, Option<ResponseCallback>);
 
-impl ResponseCallbacks {}
+#[derive(Debug, Default)]
+pub(crate) struct ResponseCallbacks(RwLock<Arena<Value>>);
+
+impl ResponseCallbacks {
+    pub(crate) fn insert(&self, callback: ResponseCallback) -> u32 {
+        let val = (ThreadSafeWaker::new(), Some(callback));
+
+        self.0.write().insert(val).slot()
+    }
+
+    /// Prototype
+    pub(crate) async fn do_callback(
+        &self,
+        slot: u32,
+        response: u8,
+        reader: CountedReader<'_>,
+    ) -> io::Result<()> {
+        let callback = match self.0.write().get_by_slot_mut(slot) {
+            None => return Ok(()),
+            Some((_index, value)) => value.1.take(),
+        };
+
+        if let Some(mut callback) = callback {
+            callback.call(response, reader).await?;
+        }
+
+        match self.0.read().get_by_slot(slot) {
+            None => return Ok(()),
+            Some((_index, value)) => value.0.done(),
+        };
+
+        Ok(())
+    }
+
+    /// Return false if slot is invalid.
+    pub(crate) fn remove(&self, slot: u32) -> bool {
+        self.0.write().remove_by_slot(slot).is_some()
+    }
+}
