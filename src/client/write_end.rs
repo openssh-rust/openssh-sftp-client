050d6b945029afa22b66834fcc01302d80ba947c
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-29 16:50:38 +1100

    Update WriteEnd: Impl `writev` (vectorized write)
    so that if the user is going to upload a huge file, we don't have to
    copy that into our own buffer.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/client/write_end.rs
+++ src/client/write_end.rs
@@ -1,13 +1,15 @@
 use core::future::Future;
 use core::pin::Pin;
+use core::slice;
 use core::task::{Context, Poll};
 
-use std::io;
+use std::io::{self, IoSlice};
 
 use tokio::sync::RwLock;
 
 use tokio::io::AsyncWriteExt;
-use tokio_pipe::{AtomicWriteBuffer, PipeWrite};
+use tokio_async_write_utility::AsyncWriteUtility;
+use tokio_pipe::{AtomicWriteIoSlices, PipeWrite};
 
 #[derive(Debug)]
 pub(crate) struct WriteEnd(RwLock<PipeWrite>);
@@ -17,33 +19,46 @@ impl WriteEnd {
         Self(RwLock::new(writer))
     }
 
-    async fn write_atomic(&self, buf: AtomicWriteBuffer<'_>) -> io::Result<()> {
-        struct AtomicWriteFuture<'a, 'b>(&'a PipeWrite, AtomicWriteBuffer<'b>);
+    async fn writev_atomic(&self, bufs: AtomicWriteIoSlices<'_, '_>) -> io::Result<()> {
+        struct AtomicWriteFuture<'a, 'b, 'c>(&'a PipeWrite, AtomicWriteIoSlices<'b, 'c>);
 
-        impl Future for AtomicWriteFuture<'_, '_> {
+        impl Future for AtomicWriteFuture<'_, '_, '_> {
             type Output = io::Result<usize>;
 
             fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-                Pin::new(self.0).poll_write_atomic(cx, self.1)
+                //Pin::new(self.0).poll_write_vectored_atomic(cx, self.1)
+                todo!()
             }
         }
 
-        let bytes = AtomicWriteFuture(&*self.0.read().await, buf).await?;
-        if bytes != buf.into_inner().len() {
-            panic!("tokio_pipe::PipeWrite::poll_write_atomic isn't atomic")
+        let _bytes = AtomicWriteFuture(&*self.0.read().await, bufs).await?;
+
+        #[cfg(debug)]
+        {
+            let expected_bytes: usize = bufs.into_inner().iter().map(|slice| slice.len()).sum();
+            if _bytes != expected_bytes {
+                panic!(
+                    "write_atomic(input len = {}), bytes writen {} which is not atomic",
+                    expected_bytes, _bytes
+                );
+            }
         }
 
         Ok(())
     }
 
-    async fn write_locked(&self, buf: &[u8]) -> io::Result<()> {
-        self.0.write().await.write_all(buf).await
+    async fn writev_locked(&self, bufs: &mut [IoSlice<'_>]) -> io::Result<()> {
+        if bufs.is_empty() {
+            return Ok(());
+        }
+
+        self.0.write().await.write_vectored_all(bufs).await
     }
 
-    pub(crate) async fn write(&self, buf: &[u8]) -> io::Result<()> {
-        match AtomicWriteBuffer::new(buf) {
-            Some(atomic_buf) => self.write_atomic(atomic_buf).await,
-            None => self.write_locked(buf).await,
+    pub(crate) async fn writev(&self, bufs: &mut [IoSlice<'_>]) -> io::Result<()> {
+        match AtomicWriteIoSlices::new(bufs) {
+            Some(atomic_bufs) => self.writev_atomic(atomic_bufs).await,
+            None => self.writev_locked(bufs).await,
         }
     }
 }
