89b23b54f1862a6b8107a5b148b903fb6f1d112d
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-29 23:33:11 +1100

    Add variant `Request::Write`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/request.rs
+++ src/request.rs
@@ -37,6 +37,25 @@ pub(crate) enum Request<'a> {
         offset: u64,
         len: u32,
     },
+
+    /// The write will extend the file if writing beyond the end of the file.
+    ///
+    /// It is legal to write way beyond the end of the file, the semantics
+    /// are to write zeroes from the end of the file to the specified offset
+    /// and then the data.
+    ///
+    /// On most operating systems, such writes do not allocate disk space but
+    /// instead leave "holes" in the file.
+    ///
+    /// The server responds to a write request with a SSH_FXP_STATUS message.
+    ///
+    /// The Write also includes any amount of custom data and its size is
+    /// included in the size of the entire packet sent.
+    Write {
+        request_id: u32,
+        handle: &'a str,
+        offset: u64,
+    },
 }
 impl Serialize for Request<'_> {
     fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
@@ -58,6 +77,11 @@ impl Serialize for Request<'_> {
             } => {
                 (constants::SSH_FXP_READ, *request_id, *handle, *offset, *len).serialize(serializer)
             }
+            Write {
+                request_id,
+                handle,
+                offset,
+            } => (constants::SSH_FXP_WRITE, *request_id, *handle, *offset).serialize(serializer),
         }
     }
 }
