5950def45bc92abf1bbfaf134f41065e8bc65df1
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-30 20:25:44 +1100

    `impl Deserialize for Response`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/response.rs
+++ src/response.rs
@@ -1,6 +1,8 @@
 use super::{constants, Extensions};
 
-use serde::de::{Deserialize, Deserializer, Error, Unexpected};
+use core::fmt;
+
+use serde::de::{Deserialize, Deserializer, Error, SeqAccess, Unexpected, Visitor};
 use ssh_format::from_bytes;
 
 use vec_strings::Strings;
@@ -51,6 +53,68 @@ pub(crate) enum ResponseInner {
 pub(crate) struct Response {
     pub(crate) response_id: u32,
     pub(crate) response_inner: ResponseInner,
+}
+
+impl<'de> Deserialize<'de> for Response {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        struct ResponseVisitor(usize);
+
+        impl ResponseVisitor {
+            fn get_next<'de, T, V>(&mut self, seq: &mut V) -> Result<T, V::Error>
+            where
+                T: Deserialize<'de>,
+                V: SeqAccess<'de>,
+            {
+                let res = seq
+                    .next_element()?
+                    .ok_or_else(|| Error::invalid_length(self.0, self));
+                self.0 += 1;
+                res
+            }
+        }
+
+        impl<'de> Visitor<'de> for ResponseVisitor {
+            type Value = Response;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                write!(formatter, "Expects a u8 type and payload")
+            }
+
+            fn visit_seq<V>(mut self, mut seq: V) -> Result<Self::Value, V::Error>
+            where
+                V: SeqAccess<'de>,
+            {
+                use constants::*;
+                use ResponseInner::*;
+
+                let discriminant: u8 = self.get_next(&mut seq)?;
+                let response_id: u32 = self.get_next(&mut seq)?;
+
+                let response_inner = match discriminant {
+                    SSH_FXP_STATUS => Status {
+                        status_code: self.get_next(&mut seq)?,
+                        err_msg: self.get_next(&mut seq)?,
+                        language_tag: self.get_next(&mut seq)?,
+                    },
+
+                    _ => {
+                        return Err(Error::invalid_value(
+                            Unexpected::Unsigned(discriminant as u64),
+                            &"Invalid packet type",
+                        ))
+                    }
+                };
+
+                Ok(Response {
+                    response_id,
+                    response_inner,
+                })
+            }
+        }
+
+        // Pass a dummy size here since ssh_format doesn't care
+        deserializer.deserialize_tuple(3, ResponseVisitor(0))
+    }
 }
 
 #[derive(Debug)]
