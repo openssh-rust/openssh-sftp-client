79ac7466477fdda99877d2645f42b8456779a245
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-30 21:00:06 +1100

    Add doc for field `NameEntry::longname`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/response.rs
+++ src/response.rs
@@ -203,6 +203,20 @@ impl<'de> Deserialize<'de> for StatusCode {
 #[derive(Debug, Deserialize, Clone)]
 pub(crate) struct NameEntry {
     filename: String,
+
+    /// The format of the `longname' field is unspecified by this protocol.
+    ///
+    /// It MUST be suitable for use in the output of a directory listing
+    /// command (in fact, the recommended operation for a directory listing
+    /// command is to simply display this data).
+    ///
+    /// However, clients SHOULD NOT attempt to parse the longname field for file
+    /// attributes, they SHOULD use the attrs field instead.
+    ///
+    /// The recommended format for the longname field is as follows:
+    ///
+    /// -rwxr-xr-x   1 mjos     staff      348911 Mar 25 14:29 t-filexfer
+    /// 1234567890 123 12345678 12345678 12345678 123456789012
     longname: String,
     attrs: FileAttrs,
 }
