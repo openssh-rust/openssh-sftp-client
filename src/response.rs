de4408a87e33ecda24433ead9ee537e81a1bad3e
Jiahao XU <Jiahao_XU@outlook.com>
2021-11-30 17:55:07 +1100

    Impl `ServerVersion::deserialize`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/response.rs
+++ src/response.rs
@@ -1,8 +1,36 @@
 use super::{constants, Extensions};
 
+use ssh_format::from_bytes;
+
+use vec_strings::Strings;
+
 pub(crate) struct ServerVersion {
     version: u32,
     extensions: Extensions,
+}
+impl ServerVersion {
+    /// * `bytes` - should not include the initial 4-byte which server
+    ///   as the length of the whole packet.
+    pub(crate) fn deserialize(bytes: &[u8]) -> ssh_format::Result<Self> {
+        let (version, mut bytes) = from_bytes(bytes)?;
+
+        let mut strings = Strings::new();
+        while !bytes.is_empty() {
+            let (string, bytes_left) = from_bytes(bytes)?;
+            strings.push(string);
+
+            bytes = bytes_left;
+        }
+
+        if let Some(extensions) = Extensions::new(strings) {
+            Ok(Self {
+                version,
+                extensions,
+            })
+        } else {
+            Err(ssh_format::Error::Eof)
+        }
+    }
 }
 
 #[derive(Debug)]
