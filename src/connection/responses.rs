ea587d0a5f21446532086ceaa6a9f58eceb5ba0e
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-01 23:36:48 +1100

    Refactor responses.rs and improving naming
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/connection/responses.rs
+++ src/connection/responses.rs
@@ -10,21 +10,17 @@ use thunderdome::Arena;
 
 pub(crate) type Value = Awaitable<(Response, Box<[u8]>)>;
 
-// TODO: Simplify this
-
 #[derive(Debug, Default)]
 pub(crate) struct Responses(Arena<Value>);
 
 impl Responses {
-    fn insert_impl(&mut self) -> (u32, Value) {
-        let val = Awaitable::new();
+    pub fn insert(&mut self) -> AwaitableResponse {
+        let awaitable_response = Awaitable::new();
 
-        (self.0.insert(val.clone()).slot(), val)
-    }
-
-    pub fn insert(&mut self) -> SlotGuard {
-        let (slot, response) = self.insert_impl();
-        SlotGuard(slot, response)
+        AwaitableResponse(
+            self.0.insert(awaitable_response.clone()).slot(),
+            awaitable_response,
+        )
     }
 
     pub(crate) async fn do_callback(
@@ -42,14 +38,16 @@ impl Responses {
 
     /// Precondition: There must not be an ongoing request for `slot`.
     pub(crate) fn remove(&mut self, slot: u32) -> Option<Value> {
-        self.0.remove_by_slot(slot).map(|(_index, value)| value)
+        self.0
+            .remove_by_slot(slot)
+            .map(|(_index, awaitable_response)| awaitable_response)
     }
 }
 
 #[derive(Debug)]
-pub(crate) struct SlotGuard(u32, Value);
+pub(crate) struct AwaitableResponse(u32, Value);
 
-impl SlotGuard {
+impl AwaitableResponse {
     pub(crate) fn get_slot_id(&self) -> u32 {
         self.0
     }
