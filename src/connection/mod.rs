b76a881ed9c2c9e585806da1daf01926e489d2c9
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-01 23:38:58 +1100

    Update use of `mod responses` in `mod connection`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,43 +1,24 @@
 mod awaitable;
 mod responses;
 
-use responses::Responses;
-
-pub(crate) use responses::{SlotGuard, SlotGuardNoAwait};
+use responses::*;
 
 use openssh_sftp_protocol::response::Response;
 
-use std::io;
-use std::sync::Arc;
-
-use tokio::task::JoinHandle;
 use tokio_pipe::{PipeRead, PipeWrite};
 
 #[derive(Debug)]
 pub struct Connection {
     writer: PipeWrite,
     reader: PipeRead,
-
-    responses: Arc<Responses>,
+    responses: Responses,
 }
 impl Connection {
-    pub(crate) fn get_request_id(&self) -> SlotGuard<'_> {
-        self.responses.insert()
-    }
-
-    /// Return a SlotGuardNoAwait that is automatically removed
-    /// when the request is done.
-    pub(crate) fn get_request_id_no_await(&self) -> SlotGuardNoAwait<'_> {
-        self.responses.insert_no_await()
-    }
-
     pub(crate) async fn new(reader: PipeRead, writer: PipeWrite) -> Self {
-        let responses = Arc::new(Responses::default());
-
         Self {
             reader,
             writer,
-            responses,
+            responses: Responses::default(),
         }
     }
 }
