3b670cd1ed01d9635d190aafca3d45717547ac12
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-02 00:20:31 +1100

    Impl `Connection::negotiate`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,24 +1,81 @@
 mod awaitable;
 mod awaitable_responses;
 
+use super::Error;
+
+use std::io::IoSlice;
+
 use awaitable_responses::*;
 
-use openssh_sftp_protocol::response::Response;
+use openssh_sftp_protocol::constants::SSH2_FILEXFER_VERSION;
+use openssh_sftp_protocol::request::{Hello, RequestInner};
+use openssh_sftp_protocol::response::{Response, ServerVersion};
+use openssh_sftp_protocol::serde::Deserialize;
+use openssh_sftp_protocol::ssh_format::Transformer;
 
+use tokio::io::AsyncReadExt;
+use tokio_async_write_utility::AsyncWriteUtility;
 use tokio_pipe::{PipeRead, PipeWrite};
 
 #[derive(Debug)]
 pub struct Connection {
     writer: PipeWrite,
     reader: PipeRead,
+    transformer: Transformer,
     responses: AwaitableResponses,
 }
 impl Connection {
-    pub(crate) async fn new(reader: PipeRead, writer: PipeWrite) -> Self {
-        Self {
+    async fn read_exact(&mut self, size: usize) -> Result<(), Error> {
+        self.transformer.get_buffer().resize(size, 0);
+        self.reader
+            .read_exact(&mut self.transformer.get_buffer())
+            .await?;
+
+        Ok(())
+    }
+
+    async fn read_and_deserialize<'a, T>(&'a mut self, size: usize) -> Result<T, Error>
+    where
+        T: Deserialize<'a>,
+    {
+        self.read_exact(size).await?;
+
+        // Ignore any trailing bytes to be forward compatible
+        Ok(self.transformer.deserialize()?.0)
+    }
+
+    async fn negotiate(&mut self) -> Result<(), Error> {
+        // Sent hello message
+        let bytes = self.transformer.serialize(Hello {
+            version: SSH2_FILEXFER_VERSION as u32,
+            extensions: Default::default(),
+        })?;
+        self.writer
+            .write_vectored_all(&mut [IoSlice::new(bytes)])
+            .await?;
+
+        // Receive server version
+        let len: u32 = self.read_and_deserialize(4).await?;
+        self.read_exact(len as usize).await?;
+        let server_version = ServerVersion::deserialize(self.transformer.get_buffer())?;
+
+        if server_version.version != (SSH2_FILEXFER_VERSION as u32) {
+            Err(Error::UnsupportedSftpProtocol)
+        } else {
+            Ok(())
+        }
+    }
+
+    pub async fn new(reader: PipeRead, writer: PipeWrite) -> Result<Self, Error> {
+        let mut val = Self {
             reader,
             writer,
+            transformer: Transformer::default(),
             responses: AwaitableResponses::default(),
-        }
+        };
+
+        val.negotiate().await?;
+
+        Ok(val)
     }
 }
