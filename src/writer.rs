e5109179069ecaac56f3d5bfd33969e1df438f65
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-26 14:28:42 +1100

    Optimize `Writer`: Take advantage of atomic write of `PipeWrite`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/writer.rs
+++ src/writer.rs
@@ -1,26 +1,83 @@
+use std::cmp::max;
+
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
 use std::io;
 
 use tokio::io::AsyncWriteExt;
-use tokio::sync::Mutex;
+use tokio::sync::RwLock;
 use tokio_io_utility::write_vectored_all;
-use tokio_pipe::PipeWrite;
+use tokio_pipe::{AtomicWriteBuffer, PipeWrite, PIPE_BUF};
+
+const MAX_ATOMIC_SIZE: usize = PIPE_BUF / 2 + PIPE_BUF / 3;
+const MAX_ATOMIC_ATTEMPT: u16 = 50;
 
 #[derive(Debug)]
-pub(crate) struct Writer(Mutex<PipeWrite>);
+pub(crate) struct Writer(RwLock<PipeWrite>);
 
 impl Writer {
     pub(crate) fn new(pipe_write: PipeWrite) -> Self {
-        Self(Mutex::new(pipe_write))
+        Self(RwLock::new(pipe_write))
     }
 
+    async fn atomic_write_all(
+        &self,
+        buf: AtomicWriteBuffer<'_>,
+    ) -> Result<Option<usize>, io::Error> {
+        #[must_use = "futures do nothing unless you `.await` or poll them"]
+        struct AtomicWrite<'a, 'b>(&'a PipeWrite, AtomicWriteBuffer<'b>, u16, u16);
+
+        impl Future for AtomicWrite<'_, '_> {
+            type Output = Option<Result<usize, io::Error>>;
+
+            fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+                if self.2 >= self.3 {
+                    return Poll::Ready(None);
+                }
+
+                self.2 += 1;
+
+                let writer = Pin::new(self.0);
+                let buf = self.1;
+
+                writer.poll_write_atomic(cx, buf).map(Some)
+            }
+        }
+
+        AtomicWrite(
+            &*self.0.read().await,
+            buf,
+            0,
+            max(
+                // PIPE_BUF is 4096, less than u16::MAX,
+                // so the result of division must also be less than u16::MAX.
+                (PIPE_BUF / buf.into_inner().len()) as u16,
+                MAX_ATOMIC_ATTEMPT,
+            ),
+        )
+        .await
+        .transpose()
+    }
+
+    /// * `buf` - Must not be empty
     pub(crate) async fn write_all(&self, buf: &[u8]) -> Result<(), io::Error> {
-        self.0.lock().await.write_all(buf).await
+        if let Some(buf) = AtomicWriteBuffer::new(buf) {
+            if buf.into_inner().len() <= MAX_ATOMIC_SIZE {
+                if let Some(_len) = self.atomic_write_all(buf).await? {
+                    return Ok(());
+                }
+            }
+        }
+
+        self.0.write().await.write_all(buf).await
     }
 
     pub(crate) async fn write_vectored_all(
         &self,
         bufs: &mut [io::IoSlice<'_>],
     ) -> Result<(), io::Error> {
-        write_vectored_all(&mut *self.0.lock().await, bufs).await
+        write_vectored_all(&mut *self.0.write().await, bufs).await
     }
 }
