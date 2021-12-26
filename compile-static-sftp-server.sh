9fe8b992375902f9b28ab7b3af57c23c25a47994
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-27 01:02:02 +1100

    Remove submodule static-openssh-sftp-server
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- compile-static-sftp-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-cd $(dirname `realpath $0`)
-
-# Clone all submodules
-git submodule update --init --depth 1 --recursive
-
-cd static-openssh-sftp-server
-exec ./compile-static.sh
