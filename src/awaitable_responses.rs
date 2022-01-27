c570e1d0bdb06b05b7cf36224f2fa7b6e2069ec7
Jiahao XU <Jiahao_XU@outlook.com>
2022-01-27 21:33:01 +1100

    Add inline hints to impl of fns in `awaitable_responses`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/awaitable_responses.rs
+++ src/awaitable_responses.rs
@@ -44,6 +44,7 @@ pub(crate) struct AwaitableResponses<Buffer: ToBuffer + 'static>(
 );
 
 impl<Buffer: Debug + ToBuffer + Send + Sync> AwaitableResponses<Buffer> {
+    #[inline(always)]
     pub(crate) fn new() -> Self {
         Self(Arena::with_capacity(3))
     }
@@ -53,14 +54,17 @@ impl<Buffer: Debug + ToBuffer + Send + Sync> AwaitableResponses<Buffer> {
         Id::new(self.0.insert(Awaitable::new()))
     }
 
+    #[inline]
     pub(crate) fn try_reserve(&self, new_id_cnt: u32) -> bool {
         self.0.try_reserve(new_id_cnt / (LEN as u32))
     }
 
+    #[inline]
     pub(crate) fn reserve(&self, new_id_cnt: u32) {
         self.0.reserve(new_id_cnt / (LEN as u32));
     }
 
+    #[inline]
     pub(crate) fn get(&self, slot: u32) -> Result<ArenaArc<Buffer>, Error> {
         self.0
             .get(slot)
@@ -72,15 +76,18 @@ impl<Buffer: Debug + ToBuffer + Send + Sync> AwaitableResponses<Buffer> {
 pub struct Id<Buffer: ToBuffer + Send + Sync>(pub(crate) ArenaArc<Buffer>);
 
 impl<Buffer: ToBuffer + Debug + Send + Sync> Id<Buffer> {
+    #[inline(always)]
     pub(crate) fn new(arc: ArenaArc<Buffer>) -> Self {
         Id(arc)
     }
 
+    #[inline(always)]
     pub(crate) fn into_inner(self) -> ArenaArc<Buffer> {
         self.destructure().0
     }
 }
 impl<Buffer: ToBuffer + Send + Sync> Drop for Id<Buffer> {
+    #[inline(always)]
     fn drop(&mut self) {
         ArenaArc::remove(&self.0);
     }
