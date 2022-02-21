10881237833c9324b43bfb4fb5e32286325c8d8a
Jiahao XU <Jiahao_XU@outlook.com>
2022-02-21 19:59:08 +1100

    Switch on warning for missing doc/debug impl, etc
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    rustdoc::broken_intra_doc_links,
+    rust_2018_idioms,
+    unreachable_pub
+)]
+
 mod error;
 pub use error::Error;
 
