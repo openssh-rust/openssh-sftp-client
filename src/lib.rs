8530abd974b29867c1828c8c2d45d7dec23d48f9
Jiahao XU <Jiahao_XU@outlook.com>
2022-02-21 20:01:44 +1100

    Add crate level doc
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! openssh sftp client, implements [sftp v3] accodring to
+//! [`openssh-portable/sftp-client.c`].
+//!
+//! ## Extensions
+//!
+//! This crate support the following extensions:
+//!  - limits
+//!  - expand path
+//!  - fsync
+//!  - hardlink
+//!  - posix rename
+//!
+//! [sftp v3]: https://www.openssh.com/txt/draft-ietf-secsh-filexfer-02.txt
+//! [`openssh-portable/sftp-client.c`]: https://github.com/openssh/openssh-portable/blob/19b3d846f06697c85957ab79a63454f57f8e22d6/sftp-client.c
+
 #![warn(
     missing_docs,
     missing_debug_implementations,
