323275f42489517c43e1740dc57e81e2ecea323b
Jiahao XU <Jiahao_XU@outlook.com>
2022-02-21 19:23:04 +1100

    Fix `run_tests.sh` when `XDG_RUNTIME_DIR` is not set
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,8 @@
 
 git submodule update --init --depth 1 --recursive
 
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:/tmp}"
+
 # Remove all files in /tmp in the container
 mkdir -p $XDG_RUNTIME_DIR/openssh_sftp_client/
 rm -rf $XDG_RUNTIME_DIR/openssh_sftp_client/*
