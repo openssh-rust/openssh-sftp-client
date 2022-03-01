c4956d00d202fc77f62195337569d8d6e72ec6b2
Jiahao XU <Jiahao_XU@outlook.com>
2022-03-01 11:38:35 +1100

    Refactor: Rm workspace `child_io_to_pipe`
    Move `unsafe` code into `tests/common.rs`.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- tests/common.rs
+++ tests/common.rs
@@ -1,11 +1,39 @@
 use once_cell::sync::OnceCell;
+use std::convert::TryInto;
 use std::env;
+use std::io::Error;
+use std::os::unix::io::{AsRawFd, RawFd};
 use std::path;
 use std::process::Stdio;
-use tokio::process;
+use tokio::process::{self, ChildStdin, ChildStdout};
 use tokio_pipe::{PipeRead, PipeWrite};
 
-use child_io_to_pipe::*;
+unsafe fn dup(raw_fd: RawFd) -> Result<RawFd, Error> {
+    let res = libc::dup(raw_fd);
+    if res == -1 {
+        Err(Error::last_os_error())
+    } else {
+        Ok(res)
+    }
+}
+
+fn child_stdin_to_pipewrite(stdin: ChildStdin) -> Result<PipeWrite, Error> {
+    let fd = stdin.as_raw_fd();
+
+    // safety: arg.as_raw_fd() is guaranteed to return a valid fd.
+    let fd = unsafe { dup(fd) }?;
+    // safety: under unix, ChildStdin/ChildStdout is implemented using pipe
+    fd.try_into()
+}
+
+fn child_stdout_to_pipewrite(stdon: ChildStdout) -> Result<PipeRead, Error> {
+    let fd = stdon.as_raw_fd();
+
+    // safety: arg.as_raw_fd() is guaranteed to return a valid fd.
+    let fd = unsafe { dup(fd) }?;
+    // safety: under unix, ChildStdin/ChildStdout is implemented using pipe
+    fd.try_into()
+}
 
 pub fn get_sftp_path() -> &'static path::Path {
     static SFTP_PATH: OnceCell<path::PathBuf> = OnceCell::new();
