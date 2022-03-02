8c1e24cb01bd93224150c602f40e1f1e10641212
Jiahao XU <Jiahao_XU@outlook.com>
2022-03-03 02:34:01 +1100

    Enable dep `tokio-pipe` only on unix
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/writer.rs
+++ src/writer.rs
@@ -9,7 +9,6 @@ use std::task::{Context, Poll};
 
 use tokio::io::AsyncWrite;
 use tokio_io_utility::queue::Buffers;
-use tokio_pipe::{AtomicWriteIoSlices, PipeWrite, PIPE_BUF};
 
 /// trait for Writer that can be used as output for sftp client.
 pub trait Writer: AsyncWrite + Unpin {
@@ -63,27 +62,33 @@ pub trait Writer: AsyncWrite + Unpin {
     }
 }
 
-impl Writer for PipeWrite {
-    const MAX_ATOMIC_WRITE_LEN: usize = PIPE_BUF;
+#[cfg(unix)]
+mod pipe_write_implementation {
+    use super::*;
+    use tokio_pipe::{AtomicWriteIoSlices, PipeWrite, PIPE_BUF};
 
-    fn poll_write_vectored_atomic(
-        &self,
-        cx: &mut Context<'_>,
-        bufs: &[IoSlice<'_>],
-    ) -> Poll<io::Result<()>> {
-        match AtomicWriteIoSlices::new(bufs) {
-            Some(bufs) if bufs.get_total_len() != 0 => Pin::new(self)
-                .poll_write_vectored_atomic(cx, bufs)
-                .map(|res| {
-                    let n = res?;
-                    if n == 0 {
-                        Err(io::Error::new(io::ErrorKind::WriteZero, ""))
-                    } else {
-                        debug_assert_eq!(n, bufs.get_total_len());
-                        Ok(())
-                    }
-                }),
-            _ => Poll::Ready(Ok(())),
+    impl Writer for PipeWrite {
+        const MAX_ATOMIC_WRITE_LEN: usize = PIPE_BUF;
+
+        fn poll_write_vectored_atomic(
+            &self,
+            cx: &mut Context<'_>,
+            bufs: &[IoSlice<'_>],
+        ) -> Poll<io::Result<()>> {
+            match AtomicWriteIoSlices::new(bufs) {
+                Some(bufs) if bufs.get_total_len() != 0 => Pin::new(self)
+                    .poll_write_vectored_atomic(cx, bufs)
+                    .map(|res| {
+                        let n = res?;
+                        if n == 0 {
+                            Err(io::Error::new(io::ErrorKind::WriteZero, ""))
+                        } else {
+                            debug_assert_eq!(n, bufs.get_total_len());
+                            Ok(())
+                        }
+                    }),
+                _ => Poll::Ready(Ok(())),
+            }
         }
     }
 }
