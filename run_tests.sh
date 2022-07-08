079b39b93db5b09a37cde1f9a1a8f2bc412a5f52
Jiahao XU <Jiahao_XU@outlook.com>
2022-07-08 14:04:57 +1000

    Rm `mkdir`/`rm -rf` in `run_tests.sh`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -8,10 +8,6 @@ git submodule update --init --depth 1 --recursive
 
 export RUNTIME_DIR=${XDG_RUNTIME_DIR:-/tmp}
 
-# Remove all files in /tmp in the container
-mkdir -p "$RUNTIME_DIR"/openssh_sftp_client/
-rm -rf "$RUNTIME_DIR"/openssh_sftp_client/*
-
 for workspace in openssh-sftp-error openssh-sftp-client-lowlevel; do
     cd "$workspace"
     cargo test "$@" --all-features
