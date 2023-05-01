cdb72bb5a72031e5449dd78a90e9cd426ae88a59
Jiahao XU <Jiahao_XU@outlook.com>
2023-05-02 01:21:38 +1000

    Refactor: Extract new fn `WriteEndWithCachedId::cancel_if_task_failed_inner`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/cache.rs
+++ src/cache.rs
@@ -9,7 +9,7 @@ use std::{
 #[derive(Debug)]
 pub(super) struct WriteEndWithCachedId<'s> {
     sftp: &'s Sftp,
-    inner: WriteEnd,
+    pub(super) inner: WriteEnd,
     id: Option<Id>,
 }
 
@@ -64,29 +64,29 @@ impl<'s> WriteEndWithCachedId<'s> {
         F: Future<Output = Result<R, E>> + Send,
         E: Into<Error> + Send,
     {
-        async fn inner<R>(
-            this: &mut WriteEndWithCachedId<'_>,
-            future: Pin<&mut (dyn Future<Output = Result<R, Error>> + Send)>,
-        ) -> Result<R, Error> {
-            let cancel_err = || Err(BoxedWaitForCancellationFuture::cancel_error());
-            let auxiliary = this.sftp.auxiliary();
-
-            let cancel_token = &auxiliary.cancel_token;
-
-            if cancel_token.is_cancelled() {
-                return cancel_err();
-            }
-
-            tokio::select! {
-                res = future => res,
-                _ = cancel_token.cancelled() => cancel_err(),
-            }
-        }
-
         let future = async move { future.await.map_err(Into::into) };
         tokio::pin!(future);
 
-        inner(self, future).await
+        self.cancel_if_task_failed_inner(future).await
+    }
+
+    pub(super) async fn cancel_if_task_failed_inner<R>(
+        &mut self,
+        future: Pin<&mut (dyn Future<Output = Result<R, Error>> + Send)>,
+    ) -> Result<R, Error> {
+        let cancel_err = || Err(BoxedWaitForCancellationFuture::cancel_error());
+        let auxiliary = self.sftp.auxiliary();
+
+        let cancel_token = &auxiliary.cancel_token;
+
+        if cancel_token.is_cancelled() {
+            return cancel_err();
+        }
+
+        tokio::select! {
+            res = future => res,
+            _ = cancel_token.cancelled() => cancel_err(),
+        }
     }
 
     pub(super) fn get_auxiliary(&self) -> &'s Auxiliary {
@@ -115,7 +115,7 @@ impl<'s> WriteEndWithCachedId<'s> {
             future: Pin<&mut (dyn Future<Output = Result<(Id, R), Error>> + Send)>,
         ) -> Result<R, Error> {
             // Requests is already added to write buffer, so wakeup
-            // the `flush_task`.
+            // the `flush_task` if necessary.
             this.get_auxiliary().wakeup_flush_task();
 
             let (id, ret) = this.cancel_if_task_failed(future).await?;
