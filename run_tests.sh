379f8d1656dad994da2b55ada424550b31696159
Jiahao XU <Jiahao_XU@outlook.com>
2023-05-05 20:10:53 +1000

    Simplify `run_tests.sh` using `--workspace` flag
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -6,10 +6,4 @@ cd "$(dirname "$(realpath "$0")")"
 
 export RUNTIME_DIR=${XDG_RUNTIME_DIR:-/tmp}
 
-for workspace in openssh-sftp-error openssh-sftp-client-lowlevel; do
-    cd "$workspace"
-    cargo test --all-features "$@"
-    cd ..
-done
-
-exec cargo test --all-features "$@"
+exec cargo test --all-features --workspace "$@"
