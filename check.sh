5b6a93ce62c0decf42c87fb322eb5df2050caf29
Jiahao XU <Jiahao_XU@outlook.com>
2023-05-17 18:53:45 +1000

    Improve CI: Speedup & more tests, and fixed one clippy warning
     - Speedup job `os-check`: Run on Linux
       Windows CI is just way too slow and this crate does not use anything
       that requires C/C++ compiler, so we can trivally cross-compile it.
     - Test features-powerset in job `os-check`
     - Turn warnings into errors
     - Add concurrency group to cancel CI for outdated commits
     - Make sure each job has its own unique cache key (for each matrix)
       and could restore from previous (`Cargo.lock`) or other jobs'
       cache as a fallback.
     - Fix docs.rs: Enable feature tracing
     - Speedup `check.sh`: Run fmt, clippy, doc once instead of multiple
       times
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- check.sh
+++ check.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-run_check() {
-    cargo fmt --all -- --check
-    cargo clippy --all-features --all --no-deps
-    cargo doc --no-deps
-}
-
 set -euxo pipefail
 
 cd "$(dirname "$(realpath "$0")")"
 
-for workspace in openssh-sftp-error openssh-sftp-client-lowlevel; do
-    cd "$workspace"
-    run_check
-    cd ..
-done
-
-run_check
+cargo fmt --all -- --check
+cargo clippy --all-features --all --no-deps
 
 export RUSTDOCFLAGS="--cfg docsrs"
-exec cargo +nightly doc --no-deps --features openssh
+exec cargo +nightly doc \
+    --no-deps \
+    --features openssh,tracing \
+    --package openssh-sftp-client \
+    --package openssh-sftp-error \
+    --package openssh-sftp-client-lowlevel
