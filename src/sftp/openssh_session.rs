bef9b21f8418ca91698705366babe5fd716f920c
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2024-07-11 19:50:42 +1000

    Implement `Sftp::from_clonable_session*` (#131)
    Fixed #130
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/sftp/openssh_session.rs
+++ src/sftp/openssh_session.rs
@@ -1,4 +1,4 @@
-use std::{future::Future, pin::Pin, sync::Arc};
+use std::{fmt::Debug, future::Future, ops::Deref, pin::Pin, sync::Arc};
 
 use openssh::{ChildStdin, ChildStdout, Error as OpensshError, Session, Stdio};
 use tokio::{sync::oneshot, task::JoinHandle};
@@ -46,15 +46,14 @@ impl Drop for OpensshSession {
     tracing::instrument(name = "session_task", skip(tx, check_openssh_connection))
 )]
 async fn create_session_task(
-    session: Session,
+    session: impl Deref<Target = Session> + Clone + Debug + Send + Sync,
     tx: oneshot::Sender<Result<(ChildStdin, ChildStdout), OpensshError>>,
     check_openssh_connection: Option<Box<dyn CheckOpensshConnection + Send + Sync>>,
 ) -> Option<Error> {
     #[cfg(feature = "tracing")]
     tracing::info!("Connecting to sftp subsystem, session = {session:?}");
 
-    let res = session
-        .subsystem("sftp")
+    let res = Session::to_subsystem(session.clone(), "sftp")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::null())
@@ -132,21 +131,7 @@ async fn create_session_task(
         );
     }
 
-    let _session_str = format!("{session:?}");
-    let occuring_error = session.close().await.err().map(Error::from);
-
-    #[cfg(feature = "tracing")]
-    if let Some(err) = &occuring_error {
-        tracing::error!("Closing session failed: {err}, session = {_session_str}");
-    }
-
-    match (original_error, occuring_error) {
-        (Some(original_error), Some(occuring_error)) => {
-            Some(original_error.error_on_cleanup(occuring_error))
-        }
-        (Some(err), None) | (None, Some(err)) => Some(err),
-        (None, None) => None,
-    }
+    original_error
 }
 
 impl Sftp {
@@ -156,10 +141,7 @@ impl Sftp {
     /// would also await on [`openssh::RemoteChild::wait`] and
     /// [`openssh::Session::close`] and propagate their error in
     /// [`Sftp::close`].
-    pub async fn from_session(
-        session: openssh::Session,
-        options: SftpOptions,
-    ) -> Result<Self, Error> {
+    pub async fn from_session(session: Session, options: SftpOptions) -> Result<Self, Error> {
         Self::from_session_with_check_connection_inner(session, options, None).await
     }
 
@@ -169,7 +151,6 @@ impl Sftp {
     /// # Example
     ///
     /// ```rust,no_run
-    ///
     /// fn check_connection<'session>(
     ///     session: &'session openssh::Session,
     /// ) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<(), openssh::Error>> + Send + Sync + 'session>> {
@@ -181,6 +162,7 @@ impl Sftp {
     ///         Ok(())
     ///     })
     /// }
+    ///
     /// # #[tokio::main(flavor = "current_thread")]
     /// # async fn main() -> Result<(), openssh_sftp_client::Error> {
     /// openssh_sftp_client::Sftp::from_session_with_check_connection(
@@ -192,7 +174,7 @@ impl Sftp {
     /// # }
     /// ```
     pub async fn from_session_with_check_connection(
-        session: openssh::Session,
+        session: Session,
         options: SftpOptions,
         check_openssh_connection: impl CheckOpensshConnection + Send + Sync + 'static,
     ) -> Result<Self, Error> {
@@ -205,14 +187,109 @@ impl Sftp {
     }
 
     async fn from_session_with_check_connection_inner(
-        session: openssh::Session,
+        session: Session,
         options: SftpOptions,
         check_openssh_connection: Option<Box<dyn CheckOpensshConnection + Send + Sync>>,
     ) -> Result<Self, Error> {
         let (tx, rx) = oneshot::channel();
 
-        let handle = tokio::spawn(create_session_task(session, tx, check_openssh_connection));
+        Self::from_session_task(
+            options,
+            rx,
+            tokio::spawn(async move {
+                let original_error =
+                    create_session_task(&session, tx, check_openssh_connection).await;
 
+                let _session_str = format!("{session:?}");
+                let occuring_error = session.close().await.err().map(Error::from);
+
+                #[cfg(feature = "tracing")]
+                if let Some(err) = &occuring_error {
+                    tracing::error!("Closing session failed: {err}, session = {_session_str}");
+                }
+
+                match (original_error, occuring_error) {
+                    (Some(original_error), Some(occuring_error)) => {
+                        Some(original_error.error_on_cleanup(occuring_error))
+                    }
+                    (Some(err), None) | (None, Some(err)) => Some(err),
+                    (None, None) => None,
+                }
+            }),
+        )
+        .await
+    }
+
+    /// Create [`Sftp`] from any type that can be dereferenced to [`openssh::Session`]
+    /// and is clonable.
+    pub async fn from_clonable_session(
+        session: impl Deref<Target = Session> + Clone + Debug + Send + Sync + 'static,
+        options: SftpOptions,
+    ) -> Result<Self, Error> {
+        Self::from_clonable_session_with_check_connection_inner(session, options, None).await
+    }
+
+    /// Similar to [`Sftp::from_session_with_check_connection`], but takes an additional parameter
+    /// for checking if the connection is still alive.
+    ///
+    /// # Example
+    ///
+    /// ```rust,no_run
+    /// fn check_connection<'session>(
+    ///     session: &'session openssh::Session,
+    /// ) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<(), openssh::Error>> + Send + Sync + 'session>> {
+    ///     Box::pin(async move {
+    ///         loop {
+    ///             tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+    ///             session.check().await?;
+    ///         }
+    ///         Ok(())
+    ///     })
+    /// }
+    ///
+    /// # #[tokio::main(flavor = "current_thread")]
+    /// # async fn main() -> Result<(), openssh_sftp_client::Error> {
+    /// openssh_sftp_client::Sftp::from_clonable_session_with_check_connection(
+    ///     std::sync::Arc::new(openssh::Session::connect_mux("me@ssh.example.com", openssh::KnownHosts::Strict).await?),
+    ///     openssh_sftp_client::SftpOptions::default(),
+    ///     check_connection,
+    /// ).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub async fn from_clonable_session_with_check_connection(
+        session: impl Deref<Target = Session> + Clone + Debug + Send + Sync + 'static,
+        options: SftpOptions,
+        check_openssh_connection: impl CheckOpensshConnection + Send + Sync + 'static,
+    ) -> Result<Self, Error> {
+        Self::from_clonable_session_with_check_connection_inner(
+            session,
+            options,
+            Some(Box::new(check_openssh_connection)),
+        )
+        .await
+    }
+
+    async fn from_clonable_session_with_check_connection_inner(
+        session: impl Deref<Target = Session> + Clone + Debug + Send + Sync + 'static,
+        options: SftpOptions,
+        check_openssh_connection: Option<Box<dyn CheckOpensshConnection + Send + Sync>>,
+    ) -> Result<Self, Error> {
+        let (tx, rx) = oneshot::channel();
+
+        Self::from_session_task(
+            options,
+            rx,
+            tokio::spawn(create_session_task(session, tx, check_openssh_connection)),
+        )
+        .await
+    }
+
+    async fn from_session_task(
+        options: SftpOptions,
+        rx: oneshot::Receiver<Result<(ChildStdin, ChildStdout), OpensshError>>,
+        handle: JoinHandle<Option<Error>>,
+    ) -> Result<Self, Error> {
         let msg = "Task failed without sending anything, so it must have panicked";
 
         let (stdin, stdout) = match rx.await {
